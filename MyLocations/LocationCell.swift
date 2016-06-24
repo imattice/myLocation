f6462b95ab7caeb80d9cdaaf9459517604b17241
Ike <ike.mattice@tiger-sheep.com>
2016-06-24 16:27:16 -0700

    wire up labels in location cell so they display the CoreData properties.

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,51 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Ike Mattice on 6/24/16.
+//  Copyright © 2016 Ike Mattice. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    func configureForLocation(location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            if let s = placemark.subThoroughfare {
+                text += s + " "
+            }
+            if let s = placemark.thoroughfare {
+                text += s + ", "
+            }
+            if let s = placemark.locality {
+                text += s
+            }
+            
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
